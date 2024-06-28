6712a0207f8b448879a3a0ef98f6341c090f2ff4
“HoseinVatani” <Hosein.vatani20@gmail.com>
2024-06-28 22:08:23 +0330

    Add interface ExtendBanService

---
+++ portal/services/user/src/main/java/com/taxi/user/service/ExtendBanService.java
@@ -0,0 +1,8 @@
+package com.taxi.user.service;
+
+import com.taxi.framework.user.service.BanService;
+import com.taxi.user.dto.ExtendedBanDTO;
+
+public interface ExtendBanService extends BanService<ExtendedBanDTO> {
+
+}
