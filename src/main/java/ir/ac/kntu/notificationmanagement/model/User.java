ce177c5dad9a589d26b48cf85df1b2e34afb2d9b
M-Erfan-Dm <erfan.dtm@gmail.com>
2024-07-05 14:22:02 +0330

    Create User model

---
+++ src/main/java/ir/ac/kntu/notificationmanagement/model/User.java
@@ -0,0 +1,25 @@
+package ir.ac.kntu.notificationmanagement.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String phoneNumber;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String pushToken;
+}
