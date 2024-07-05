cf3a7375dda1d831df133efad16b7d1a9e4554a9
M-Erfan-Dm <erfan.dtm@gmail.com>
2024-07-05 13:42:02 +0330

    Init commit

---
+++ src/main/java/ir/ac/kntu/notificationmanagement/NotificationManagementApplication.java
@@ -0,0 +1,13 @@
+package ir.ac.kntu.notificationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationManagementApplication.class, args);
+    }
+
+}
