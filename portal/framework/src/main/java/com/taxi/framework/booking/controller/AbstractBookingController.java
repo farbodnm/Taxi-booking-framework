40c3333bb40a09cb3afec3b760476590a52bc634
Alireza GHANEI <107796042+Alireza-GHANEI@users.noreply.github.com>
2024-07-06 13:11:09 +0330

    Update AbstractBookingController.java

--- portal/framework/src/main/java/com/taxi/framework/booking/controller/AbstractBookingController.java
+++ portal/framework/src/main/java/com/taxi/framework/booking/controller/AbstractBookingController.java
@@ -3,19 +3,24 @@ package com.taxi.framework.booking.controller;
 import com.taxi.framework.booking.dto.BaseBookedRequestDTO;
 import com.taxi.framework.booking.dto.BaseBookingRequestDTO;
 import com.taxi.framework.booking.service.BookingCreationService;
+import com.taxi.framework.user.dto.AccessibilitySettingsDTO;
+import com.taxi.framework.user.service.UserAccessibilityService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 public abstract class AbstractBookingController<T extends BaseBookingRequestDTO, Y extends BaseBookedRequestDTO> {
     protected final BookingCreationService<T, Y> bookingCreationService;
+    protected final UserAccessibilityService userAccessibilityService;
 
-    protected AbstractBookingController(BookingCreationService<T, Y> bookingCreationService) {
+    protected AbstractBookingController(BookingCreationService<T, Y> bookingCreationService, UserAccessibilityService userAccessibilityService) {
         this.bookingCreationService = bookingCreationService;
+        this.userAccessibilityService = userAccessibilityService;
     }
 
     @PostMapping("/book")
     public ResponseEntity<String> book(@RequestBody T dto) {
-        return ResponseEntity.ok(bookingCreationService.createBooking(dto));
+        AccessibilitySettingsDTO settings = userAccessibilityService.getAccessibilitySettings(dto.getUserId());
+        return ResponseEntity.ok(bookingCreationService.createBooking(dto, settings));
     }
 
     @PostMapping("/booked/{userId}")
