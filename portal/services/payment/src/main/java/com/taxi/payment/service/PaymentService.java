c04228f1f6a5913abb5285696b26d03f23c250a9
fnaeijii <fatemehnaeiji2002@gmail.com>
2024-07-08 14:06:14 +0330

    add service for payment module

---
+++ portal/services/payment/src/main/java/com/taxi/payment/service/PaymentService.java
@@ -0,0 +1,23 @@
+package com.taxi.payment.service;
+
+import com.taxi.framework.payment.dto.PaymentRequest;
+import com.taxi.framework.payment.dto.PaymentResponse;
+import com.taxi.framework.payment.service.AbstractPaymentService;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PaymentService extends AbstractPaymentService<PaymentRequest, PaymentResponse> {
+
+    public PaymentService(RestTemplate restTemplate) {
+        super(restTemplate);
+    }
+
+    @Override
+    protected Class<PaymentResponse> getResponseType() {
+        return PaymentResponse.class;
+    }
+
+    // Implement other specific methods if needed
+}
+
