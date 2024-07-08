2938a25395dfc783141851cb04b9122669367151
Azin Ghanavati <azinghanavati2002@gmail.com>
2024-07-08 20:29:40 +0330

    serviceImp and controller added

--- portal/services/sms/src/main/java/com/taxi/sms/service/ServiceImp.java
+++ portal/services/sms/src/main/java/com/taxi/sms/service/ServiceImp.java
@@ -1,53 +1,54 @@
 package com.taxi.sms.service;
 
-
 import com.taxi.framework.sms.dto.SmsRequestDTO;
 import com.google.gson.Gson;
 import com.taxi.framework.sms.service.AbstractSmsService;
-//-------------------------
-import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.MediaType;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.*;
-//-------------------------
-import org.apache.catalina.connector.Request;
-import org.apache.catalina.connector.Response;
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.io.IOException;
 
 @Service
 public class ServiceImp extends AbstractSmsService {
+    private final OkHttpClient client;
+    private final String apiUrl;
+    private final String apiKey;
+
     public ServiceImp(
             @Value("${sms.api.url}") String apiUrl,
             @Value("${sms.api.key}") String apiKey) {
         super(apiUrl, apiKey);
+        this.client = new OkHttpClient();
+        this.apiUrl = apiUrl;
+        this.apiKey = apiKey;
     }
 
     @Override
     public String sendSms(SmsRequestDTO smsRequest) throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        MediaType mediaType = MediaType.parse("application/json");
         Gson gson = new Gson();
         String requestBodyJson = gson.toJson(smsRequest);
-        RequestBody body = RequestBody.create(mediaType, requestBodyJson);
+
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(requestBodyJson, mediaType);
+
         Request request = new Request.Builder()
                 .url(apiUrl)
-                .method("POST", body)
+                .post(body)
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Accept", "text/plain")
                 .addHeader("x-api-key", apiKey)
                 .build();
 
-        Response response = client.newCall(request).execute();
-        if (!response.isSuccessful()) {
-            throw new IOException("Unexpected code " + response);
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+            return response.body().string();
         }
-        return response.body().string();
     }
 }
