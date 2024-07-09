a3c25987c96be4e09eca84d4db59c92dd8402931
M-Erfan-Dm <erfan.dtm@gmail.com>
2024-07-10 00:58:30 +0330

    Update CustomExceptionHandler to return MessageResponseDTO instead of String

--- src/main/java/ir/ac/kntu/notificationmanagement/exceptions/CustomExceptionHandler.java
+++ src/main/java/ir/ac/kntu/notificationmanagement/exceptions/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package ir.ac.kntu.notificationmanagement.exceptions;
 
+import ir.ac.kntu.notificationmanagement.controller.MessageResponseDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +11,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> handleResourceNotFoundException(ResourceNotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<MessageResponseDTO> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                new MessageResponseDTO(ex.getMessage()));
     }
 
     @ExceptionHandler(DuplicateValueException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(DuplicateValueException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(ex.getMessage());
+    public ResponseEntity<MessageResponseDTO> handleEntityNotFoundException(DuplicateValueException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(
+                new MessageResponseDTO(ex.getMessage()));
     }
 }
