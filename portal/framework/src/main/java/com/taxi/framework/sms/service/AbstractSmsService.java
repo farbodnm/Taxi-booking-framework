7c1c6f77227bdf48d246c0d34c7506f76b426de2
Azin Ghanavati <azinghanavati2002@gmail.com>
2024-07-09 19:06:21 +0330

    AbstractSmsService and SmsService have changed due to addition of design patterns

--- portal/framework/src/main/java/com/taxi/framework/sms/service/AbstractSmsService.java
+++ portal/framework/src/main/java/com/taxi/framework/sms/service/AbstractSmsService.java
@@ -1,16 +1,20 @@
 package com.taxi.framework.sms.service;
 
 import com.taxi.framework.sms.dto.SmsRequestDTO;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import java.io.IOException;
 
 public abstract class AbstractSmsService {
-    protected String apiUrl;
-    protected String apiKey;
-
-    public AbstractSmsService(String apiUrl, String apiKey) {
-        this.apiUrl = apiUrl;
-        this.apiKey = apiKey;
+    // Template Method Pattern: Defines the skeleton of the SMS sending algorithm
+    public String sendSms(SmsRequestDTO smsRequestDTO) throws IOException {
+        OkHttpClient client = createHttpClient();  // Factory Method for creating OkHttpClient
+        Request request = createRequest(smsRequestDTO);  // Factory Method for creating Request
+        return executeRequest(client, request);  // Concrete implementation of request execution
     }
 
-    public abstract String sendSms(SmsRequestDTO smsRequest) throws IOException;
+    // Abstract methods to be implemented by subclasses
+    protected abstract OkHttpClient createHttpClient();
+    protected abstract Request createRequest(SmsRequestDTO smsRequestDTO) throws IOException;
+    protected abstract String executeRequest(OkHttpClient client, Request request) throws IOException;
 }
--- portal/services/sms/src/main/java/com/taxi/sms/service/SmsService.java
+++ portal/services/sms/src/main/java/com/taxi/sms/service/SmsService.java
@@ -12,44 +12,87 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 public class SmsService extends AbstractSmsService {
-    private final OkHttpClient client;
-    private final String apiUrl;
     private final String apiKey;
+    private final String url;
 
-    public SmsService(
-
-            @Value("${sms.api.url}") String apiUrl,
-            @Value("${sms.api.key}") String apiKey) {
-        super(apiUrl, apiKey);
-        this.client = new OkHttpClient();
-        this.apiUrl = apiUrl;
+    public SmsService(@Value("${sms.api.key}") String apiKey, @Value("${sms.api.url}") String url) {
         this.apiKey = apiKey;
+        this.url = url;
     }
 
     @Override
-    public String sendSms(SmsRequestDTO smsRequest) throws IOException {
-        Gson gson = new Gson();
-        String requestBodyJson = gson.toJson(smsRequest);
+    protected OkHttpClient createHttpClient() {
+        return new OkHttpClient().newBuilder().build();  // Factory Method for OkHttpClient
+    }
 
+    @Override
+    protected Request createRequest(SmsRequestDTO smsRequestDTO) {
         MediaType mediaType = MediaType.parse("application/json");
-        RequestBody body = RequestBody.create(requestBodyJson, mediaType);
+        String jsonBody = new SmsRequestBodyBuilder()  // Using Builder Pattern
+                .withMobile(smsRequestDTO.getMobile())  // Adding mobile to builder
+                .withTemplateId(smsRequestDTO.getTemplateId())  // Adding templateId to builder
+                .withParameters(smsRequestDTO.getParameters())  // Adding parameters to builder
+                .build();  // Building JSON body
 
-        Request request = new Request.Builder()
-                .url(apiUrl)
+        RequestBody body = RequestBody.create(mediaType, jsonBody);
+        return new Request.Builder()
+                .url(url)
                 .post(body)
                 .addHeader("Content-Type", "application/json")
                 .addHeader("Accept", "text/plain")
                 .addHeader("x-api-key", apiKey)
                 .build();
+    }
 
+    @Override
+    protected String executeRequest(OkHttpClient client, Request request) throws IOException {
         try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new IOException("Unexpected code " + response);
+            return response.body() != null ? response.body().string() : null;
+        }
+    }
+
+    // Builder Pattern: Inner static class for building JSON request body
+    private static class SmsRequestBodyBuilder {
+        private String mobile;
+        private String templateId;
+        private List<SmsRequestDTO.Parameter> parameters;
+
+        public SmsRequestBodyBuilder withMobile(String mobile) {
+            this.mobile = mobile;
+            return this;
+        }
+
+        public SmsRequestBodyBuilder withTemplateId(String templateId) {
+            this.templateId = templateId;
+            return this;
+        }
+
+        public SmsRequestBodyBuilder withParameters(List<SmsRequestDTO.Parameter> parameters) {
+            this.parameters = parameters;
+            return this;
+        }
+
+        public String build() {
+            StringBuilder parametersJson = new StringBuilder("[");
+            for (SmsRequestDTO.Parameter parameter : parameters) {
+                parametersJson.append("{")
+                        .append("\"name\":\"").append(parameter.getName()).append("\",")
+                        .append("\"value\":\"").append(parameter.getValue()).append("\"},");
             }
-            return response.body().string();
+            if (parametersJson.length() > 1) {
+                parametersJson.setLength(parametersJson.length() - 1); // Remove trailing comma
+            }
+            parametersJson.append("]");
+
+            return "{\n" +
+                    "\"mobile\":\"" + mobile + "\",\n" +
+                    "\"templateId\":\"" + templateId + "\",\n" +
+                    "\"parameters\":" + parametersJson.toString() + "\n" +
+                    "}";
         }
     }
 }
