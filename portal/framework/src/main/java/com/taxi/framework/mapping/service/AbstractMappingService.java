c781d6c124a6783e4a0c2c419f6ef43811b48591
Maryam.s <maryam.s01382@gmail.com>
2024-07-10 23:56:11 +0330

    update AbstractMappingService.java change apiKey initial

--- portal/framework/src/main/java/com/taxi/framework/mapping/service/AbstractMappingService.java
+++ portal/framework/src/main/java/com/taxi/framework/mapping/service/AbstractMappingService.java
@@ -1,17 +1,20 @@
 package com.taxi.framework.mapping.service;
 
-import org.springframework.beans.factory.annotation.Value;
+import com.taxi.framework.mapping.dto.DirectionsRequestDTO;
+import com.taxi.framework.mapping.dto.ReverseGeocodingRequestDTO;
 import org.springframework.web.client.RestTemplate;
 
-public abstract class AbstractMappingService implements MappingService {
-
-    @Value("${map.api.key}")
-    protected String apiKey;
+public abstract class AbstractMappingService<T extends DirectionsRequestDTO, U extends ReverseGeocodingRequestDTO> implements MappingService {
 
     protected RestTemplate restTemplate;
+    protected String apiKey;
 
     public AbstractMappingService(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    public void setApiKey(String apiKey) {
+        this.apiKey = apiKey;
     }
 
     protected void validateApiKey() {
